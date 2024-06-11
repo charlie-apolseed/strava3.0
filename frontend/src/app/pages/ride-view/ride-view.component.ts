288ffc94f1277649463bb2bf7eb4d6e7bad0bea4
charlie-apolseed <capolinsky@bowdoin.edu>
2024-06-11 15:58:59 +0200

    Initializing angular

---
+++ frontend/src/app/pages/ride-view/ride-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './ride-view.component.html',
+  styleUrl: './ride-view.component.css'
+})
+export class RideViewComponent {
+
+}
