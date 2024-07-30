ef048644d5eb8f10ef0b14556f1ed0f885c369ac
charlie-apolseed <capolinsky@bowdoin.edu>
2024-07-30 15:13:37 +0200

    Empty Frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { ActivitiesListComponent } from './pages/activities-list/activities-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ {path: 'activities-list', component: ActivitiesListComponent}];
