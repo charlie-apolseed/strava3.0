4ed7c7da976f94ce52205dc00fe04654a751d5ad
Charlie Apolinsky <charlie.apolinsky@gmail.com>
2024-09-27 17:39:31 +0200

    [Implementation][Formatting] Added features to progress tracker page to get all-time and monthly totals

--- frontend/src/app/pages/progress-tracker/progress-tracker.component.ts
+++ frontend/src/app/pages/progress-tracker/progress-tracker.component.ts
@@ -6,19 +6,31 @@ import { FormsModule } from '@angular/forms';
 import { GoalsService } from '../../services/goals.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { LabelType, Options, NgxSliderModule } from '@angular-slider/ngx-slider';
+import { ActivitiesService } from '../../services/activities.service';
+import Activity from '../../models/activity';
+
 
 @Component({
   selector: 'app-progress-tracker',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterOutlet, RouterLinkActive, NgxSliderModule, FormsModule],
   templateUrl: './progress-tracker.component.html',
-  styleUrl: './progress-tracker.component.css'
+  styleUrls: ['./progress-tracker.component.css']
 })
 export class ProgressTrackerComponent {
   @ViewChild('closeBtn') closeBtn!: ElementRef;
   isBrowser: boolean;
-  allTimeDistance = 600;
-  allTimeElevation = 100;
+
+  allTimeDistance = 0;
+  allTimeElevation = 0;
+  allTimeActivities = 0;
+  allTimeTime = 0;
+  oneMonthAgoDistance = 0;
+  oneMonthAgoTime = 0;
+  oneMonthAgoElevation = 0;
+  oneMonthAgoActivities = 0;
+
+  allActivities : Activity[] = []
   allGoals: Goal[] = [
     {title: "Sample Goal 1",
     startDate: "10/10/10",
@@ -57,14 +69,34 @@ export class ProgressTrackerComponent {
   newGoalDate: string = "";
   newGoalTargetValue: number = 0;
   
+  
  
 
 
-  constructor(private goalService: GoalsService, @Inject(PLATFORM_ID) private platformId: Object) {
+  constructor(private goalService: GoalsService, private activitiesService: ActivitiesService, @Inject(PLATFORM_ID) private platformId: Object) {
     this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
   async ngOnInit(): Promise<void> {
+    this.activitiesService.uploadActivitiesToDatabase().subscribe({
+      next: (response: any) => {
+        console.log("Activities successfully updated");
+      },
+      error: (error: any) => {
+        console.error('Update failed', error);
+        if (error instanceof HttpErrorResponse && error.error instanceof SyntaxError) {
+          console.error('The server returned an invalid JSON:', error.message);
+        } else {
+          console.error('An unexpected error occurred:', error.message);
+        }
+      }      
+    });
+    this.activitiesService.getAllActivities().subscribe(activities => {
+      this.allActivities = activities;
+      this.allActivities.sort(this.dateComparator);
+      console.log("Successfully got " + this.allActivities.length + " activities")
+    })
+    this.getAllTimeTotals();
     this.getAllGoals();
     this.getActiveGoals(); 
   }
@@ -149,6 +181,44 @@ export class ProgressTrackerComponent {
   displayNewGoalForm() {
 
     this.newGoalSectionDisplayed = !this.newGoalSectionDisplayed;
+  }
+
+  getAllTimeTotals() {
+    const currentDate = new Date()
+    const oneMonthAgo = new Date();
+    oneMonthAgo.setMonth(currentDate.getMonth() - 1);
+    for (const activity of this.allActivities) {
+      this.allTimeElevation += activity.elevation
+      this.allTimeDistance += activity.distance
+      this.allTimeTime += activity.duration
+      //Calculate 1 month ago
+      if (this.validateDate(activity.date, oneMonthAgo)){
+        this.oneMonthAgoElevation += activity.elevation
+        this.oneMonthAgoDistance += activity.distance
+        this.oneMonthAgoTime += activity.duration
+        this.oneMonthAgoActivities += 1
+      }
+    }
+    //Format
+    this.allTimeTime = Math.floor(this.allTimeTime / 3600)
+    this.allTimeDistance = Math.floor(this.allTimeDistance / 1000)
+    this.allTimeElevation = Math.floor(this.allTimeElevation / 1)
+    this.allTimeActivities = this.allActivities.length
+    //Format 1-mo
+    this.oneMonthAgoTime = Math.floor(this.oneMonthAgoTime / 3600)
+    this.oneMonthAgoDistance = Math.floor(this.oneMonthAgoDistance / 1000)
+    this.oneMonthAgoElevation = Math.floor(this.oneMonthAgoElevation / 1)
+  }
+  
+  validateDate(activityDate: string, boundaryDate: Date): boolean {
+    const activityDateFormatted = new Date(activityDate)
+    return activityDateFormatted > boundaryDate
+  }
+
+  dateComparator(a: Activity, b: Activity): number {
+    const aDate = new Date(a.date);
+    const bDate = new Date(b.date);
+    return bDate.getTime() - aDate.getTime()
   }
 
 }
